71afbcd29e96c1dbc3befa73aa7bc6779091de6a
Syntaf <gmercer015@gmail.com>
2015-01-28 12:38:43 -0800

    added runtime dependencies to CMakeLists.txt

--- src/frontend/pySolve.py
+++ src/frontend/pySolve.py
@@ -5,20 +5,4 @@ fName = "mazeDataFile.txt"
 with open(fName) as f:
 		content = f.readlines()
 
-ROWS = None
-COLS = None
-
-#get rows and columns in text file from first line
-words = content[0].split(" ")
-for c in range(0,len(words)):
-	if words[c] == "ROWS":
-		ROWS = words[c+1]
-	elif words[c] == "COLS":
-		COLS = words[c+1]
-
-#create empty list
-maze_map = [[0 for x in range(int(COLS))] for x in range(int(ROWS))]
-
-#loop through and establish walls
-for h in content:
-	print(h, end="")
+print(content)
